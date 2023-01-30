9e2d65a593a5f5e8737c6e426e7a19485f7b9b00
1771245847 <55345096+1771245847@users.noreply.github.com>
2023-01-30 19:40:13 +0800

    music

---
+++ css/cms.js
@@ -0,0 +1,3 @@
+<script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script id="xplayer" src="https://music.piphp.com/Static/player/player.js" key="60411033e9c3c" m="1"></script>
